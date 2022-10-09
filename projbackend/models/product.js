643bc7f5ae72d18a926adba3f2b384fca9a73a73
khushi <khushiahuja567@gmail.com>
2022-10-09 22:18:31 +0530

    product schema added

---
+++ projbackend/models/product.js
@@ -0,0 +1,41 @@
+var mongoose = require('mongoose');
+var { ObjectId } = mongoose.Schema;
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String, 
+        trim: true,
+        required: true,
+        maxlength: 32
+    },
+    description: {
+        type: String,
+        trim: true,
+        required: true,
+        maxlength: 2000
+    },
+    price: {
+        type: Number,
+        require: true,
+        maxlength: 32,
+        trim: true
+    },
+    category: {
+        type: ObjectId,
+        ref: "Category",
+        required: true
+    },
+    stock: {
+        type: Number
+    },
+    sold: {
+        type: Number,
+        default: 0
+    },
+    photo: {
+        data: Buffer,
+        contentType: String
+    }
+}, {timestamps: true});
+
+modules.exports = mongoose.model("Product", productSchema);
